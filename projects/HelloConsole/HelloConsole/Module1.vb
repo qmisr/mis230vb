cf301eb3783d2898f412d9d7d3fe8f66bd4747e5
Mohammad AlMarzouq <m.almarzouq@kbsoft.com.kw>
2018-01-15 19:19:37 +0300

    added slides and projects

---
+++ projects/HelloConsole/HelloConsole/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myText As String
+        Console.Out.WriteLine("Please type the greeting message:")
+        myText = Console.In.ReadLine()
+        Console.Out.WriteLine(myText)
+        Console.In.ReadLine()
+    End Sub
+
+End Module
---
+++ projects/HelloWorld/HelloWorld/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Hello World!")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
